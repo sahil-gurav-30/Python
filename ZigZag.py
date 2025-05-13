a6e4ab8f12d1280d96aa4667cddfe4c6c385f4ef
sahil-gurav-30 <psahilp.24.beai@acharya.ac.in>
2025-05-13 22:02:15 +0530

    Create ZigZag.py

---
+++ ZigZag.py
@@ -0,0 +1,18 @@
+import time
+import sys
+indent = 0     
+indent_increasing = True
+try:
+    while True:
+        print(' ' * indent + '*' * 10)  
+        time.sleep(0.1)  # Pause for 0.1 seconds
+        if indent_increasing:
+            indent += 1
+            if indent == 20: 
+                indent_increasing = False
+        else:
+            indent -= 1
+            if indent == 0:
+                indent_increasing = True
+except KeyboardInterrupt:
+    sys.exit() 
