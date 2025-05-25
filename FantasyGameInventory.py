2daa919a0da388bee082727bca2495974a0abebb
sahil-gurav-30 <psahilp.24.beai@acharya.ac.in>
2025-05-25 23:11:58 +0530

    Create FantasyGameInventory.py

---
+++ FantasyGameInventory.py
@@ -0,0 +1,15 @@
+def displayInventory(inventory):
+    print("Inventory:")
+    total_items = 0
+    for item, count in inventory.items():
+        print(f"{count} {item}")
+        total_items += count
+    print(f"Total number of items: {total_items}")
+stuff = {
+    'arrow': 12,
+    'gold coin': 42,
+    'rope': 1,
+    'torch': 6,
+    'dagger': 1
+}
+displayInventory(stuff)
