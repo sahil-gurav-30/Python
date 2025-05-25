6a6b036ceaae714a0908c0749b827d1e92e367be
sahil-gurav-30 <psahilp.24.beai@acharya.ac.in>
2025-05-25 23:29:22 +0530

    Create GuessTheNumber.py

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -9,9 +9,9 @@ while True:
     print("Take a guess.")
     guess = int(input())  
     guess_count += 1
-    if guess < secret_number:
+    if guess < number:
         print("Your guess is too low.")
-    elif guess > secret_number:
+    elif guess > number:
         print("Your guess is too high.")
     else:
         print(f"Good job! You guessed my number in {guess_count} guesses!")
